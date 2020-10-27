a45205669cdb335a66f98af8c08bb4b10cff9b5c
NeyNey <drfrankenstein123@gmail.com>
2020-10-26 19:33:21 -0500

    boilerplate code

---
+++ routes/html-routes.js
@@ -0,0 +1,18 @@
+const path = requite("path");
+
+module.exports = (app) => {
+  app.get("/", (req, res) => {
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../publicsignup.html"));
+  });
+  app.get("/login" ,(req, res) => {
+    if (req, res) {
+      res.redirect("/members");
+    }
+  });
+  app.get("/members" , isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+};
