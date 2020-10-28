2d9441ed85cd427fda2b11468ea3f623a8ea88ff
Steven Rodriguez <srrodrig.rodriguez@gmail.com>
2020-10-28 15:05:08 -0500

    minor changes added

--- Seeders/seeds.sql
+++ Seeders/seeds.sql
@@ -10,7 +10,7 @@ CREATE TABLE items (
 );
 
 
-INSERT INTO bid_items (category, item_name, descript, replica, starting_bid)
-VALUES ("Weapon", "Katana", "good quality. Previously displayed at visitors center in Tokyo", true, 250), ("Weapon", "Naginata", "antique, circa 1150AD", false, 14000)
+INSERT INTO items (category, item_name, descript, replica, starting_bid)
+VALUES ("Weapon", "Katana", "good quality. Previously displayed at visitors center in Tokyo", true, 250), ("Weapon", "Naginata", "antique, circa 1150AD", false, 14000),
 ("Weapon", "Yumi", "Documented use during the Heian Period. 800 - 1000 AD", true, 16000), ("Weapon", "Tanto", "Great replica!  Hand made in late 1800's", true, 1200), 
 ("Armor", "Helmet", "Traditionally made by master craftsman", true, 125), ("Armor", "Yoroi Armour", "Very nice", true, 250), ("Armor", "Hoate", "Authentic. Family heirloom. A must have!", false, 4000);
