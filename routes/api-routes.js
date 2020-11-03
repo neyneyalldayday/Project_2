c36c1b123b94b2d0d424499d0d97eed97b43ea48
NeyNey <drfrankenstein123@gmail.com>
2020-11-03 13:29:17 -0600

    no more broken

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,7 +15,7 @@ module.exports = function(app) {
       password: req.body.password
     })
       .then(() => {
-        res.redirect(307, "/api/login");
+        res.redirect( "/");
       })
       .catch(() => {
         res.render("/signup");
