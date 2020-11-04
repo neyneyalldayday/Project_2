6b0c97a0a1a300ea45c979d9c94445c8a7f6f973
Daniel Rendon <rendod99@gmail.com>
2020-11-04 17:28:03 -0600

    Addeed debug logs

--- models/index.js
+++ models/index.js
@@ -8,7 +8,9 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 let sequelize;
+console.log(config, "config")
 if (config.use_env_variable) {
+  console.log(process.env[config.use_env_variable]);
   sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
